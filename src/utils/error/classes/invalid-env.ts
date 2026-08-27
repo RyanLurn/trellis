bac61bb06cc280205ba443997032ee01491df671
Ryan Luong <lumintriet@gmail.com>
2026-08-27 08:30:55 +0700

    feat(utils/error): define InvalidEnvError class.

---
+++ src/utils/error/classes/invalid-env.ts
@@ -0,0 +1,12 @@
+import type { ZodError } from "zod";
+
+import { BaseError } from "@/utils/error/classes/base";
+
+export class InvalidEnvError extends BaseError<"INVALID_ENV_ERROR", ZodError> {
+  readonly name = "InvalidEnvError";
+  readonly code = "INVALID_ENV_ERROR";
+
+  constructor({ message, cause }: { message?: string; cause: ZodError }) {
+    super({ message: message ?? cause.message, cause });
+  }
+}
