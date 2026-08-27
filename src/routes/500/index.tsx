455c349606c94ddfd930fda92695da8d77de6017
Ryan Luong <lumintriet@gmail.com>
2026-08-27 18:40:26 +0700

    feat(ui): define InternalServerErrorPage component.

--- src/routes/500/index.tsx
+++ src/routes/500/index.tsx
@@ -1,9 +1,31 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+
+import { buttonVariants } from "@/components/ui/button";
+import { DEFAULT_ERROR_MESSAGE } from "@/utils/error/constants";
+import { RedirectSearchParamSchema } from "@/utils/schemas/redirect";
 
 export const Route = createFileRoute("/500/")({
-  component: RouteComponent,
+  validateSearch: RedirectSearchParamSchema,
+  component: InternalServerErrorPage,
 });
 
-function RouteComponent() {
-  return <div>Hello "/500/"!</div>;
+function InternalServerErrorPage() {
+  const { redirect } = Route.useSearch();
+  const linkClassName = buttonVariants({ variant: "outline" });
+
+  return (
+    <div className="typeset flex h-full flex-col items-center justify-center gap-y-3">
+      <h1 className="text-destructive">500 - Internal server error</h1>
+      <p>{DEFAULT_ERROR_MESSAGE}</p>
+      {redirect ? (
+        <Link className={linkClassName} to={redirect}>
+          Retry
+        </Link>
+      ) : (
+        <Link className={linkClassName} to="/">
+          Back to Home page
+        </Link>
+      )}
+    </div>
+  );
 }
