d9e16dd2f2dfbd242852d5db161a4f132d8fe72f
Ryan Luong <lumintriet@gmail.com>
2026-08-27 08:33:25 +0700

    feat(auth/types): define auth types.

---
+++ src/features/auth/types.ts
@@ -0,0 +1,8 @@
+import type { createAuthServer } from "@/features/auth/server";
+
+export type AuthServer = ReturnType<typeof createAuthServer>;
+
+export type AuthErrorCode = keyof AuthServer["$ERROR_CODES"];
+
+export type AuthSession = AuthServer["$Infer"]["Session"]["session"];
+export type AuthUser = AuthServer["$Infer"]["Session"]["user"];
