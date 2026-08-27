9b9506953603fe1d0473e7b27728ae81d7745e1f
Ryan Luong <lumintriet@gmail.com>
2026-08-27 09:30:53 +0700

    feat(utils/error): simplify InvalidEnvError.

--- src/utils/error/classes/invalid-env.ts
+++ src/utils/error/classes/invalid-env.ts
@@ -2,11 +2,14 @@ import type { ZodError } from "zod";
 
 import { BaseError } from "@/utils/error/classes/base";
 
-export class InvalidEnvError extends BaseError<"INVALID_ENV_ERROR", ZodError> {
+export class InvalidEnvError extends BaseError<
+  "INVALID_ENV_ERROR",
+  ZodError["issues"]
+> {
   readonly name = "InvalidEnvError";
   readonly code = "INVALID_ENV_ERROR";
 
   constructor({ message, cause }: { message?: string; cause: ZodError }) {
-    super({ message: message ?? cause.message, cause });
+    super({ message: message ?? cause.message, cause: cause.issues });
   }
 }
