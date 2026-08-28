fbc425269b681cf97100fb120bc4d8bfbef82fb0
Ryan Luong <lumintriet@gmail.com>
2026-08-28 10:20:26 +0700

    test(utils/fetch): write validation error case for parseResponse function.

--- src/utils/fetch/response/parse.test.ts
+++ src/utils/fetch/response/parse.test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { z } from "zod";
 
+import { ValidationError } from "@/utils/error/classes/validation";
 import { parseResponse } from "@/utils/fetch/response/parse";
 
 // Happy path
@@ -13,4 +14,18 @@ test("parseResponse function should parse valid response", async () => {
 
   expect.assert(result.ok === true);
   expect(result.data).toMatchObject(body);
+});
+
+// Sad paths
+describe("parseResponse function should", async () => {
+  test("return a validation error on invalid data", async () => {
+    const body = { data: "test data" };
+    const response = Response.json(body);
+    const schema = z.object({ ok: z.literal(true), data: z.string() });
+
+    const result = await parseResponse({ response, schema });
+
+    expect.assert(result.ok === false);
+    expect(result.error).toBeInstanceOf(ValidationError);
+  });
 });
