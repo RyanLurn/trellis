54da9346641e2478961633df81757361568268c2
Ryan Luong <lumintriet@gmail.com>
2026-08-28 08:56:06 +0700

    feat(utils/fetch): rewrite createUrl function.

--- src/utils/fetch/create/url.ts
+++ src/utils/fetch/create/url.ts
@@ -1,6 +1,6 @@
 import type { Result } from "@/types/result";
 
-import { InvalidUrlError } from "@/utils/fetch/error/classes/invalid-url";
+import { UnexpectedError } from "@/utils/error/classes/unexpected";
 import { err, ok } from "@/utils/result";
 
 export function createUrl({
@@ -9,11 +9,16 @@ export function createUrl({
 }: {
   url: string | URL;
   base?: string | URL;
-}): Result<URL, InvalidUrlError> {
+}): Result<URL, UnexpectedError> {
   try {
-    const urlInstance = new URL(url, base);
-    return ok(urlInstance);
+    return ok(new URL(url, base));
   } catch (error) {
-    return err(new InvalidUrlError({ url, base, cause: error as TypeError }));
+    return err(
+      new UnexpectedError({
+        failedTo: "create URL",
+        cause: error,
+        context: { url, base },
+      }),
+    );
   }
 }
