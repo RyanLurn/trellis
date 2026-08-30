7177feef77ada7e2eaef2b166a3fc5657463fe6a
Ryan Luong <lumintriet@gmail.com>
2026-08-30 09:42:04 +0700

    feat(utils/error): define reportErrorServerFn.

---
+++ src/utils/error/report.function.ts
@@ -0,0 +1,26 @@
+import { createServerFn } from "@tanstack/react-start";
+
+import type { FlatErrorObject } from "@/types/error";
+import type { Result } from "@/types/result";
+
+import { ValidationError } from "@/utils/error/classes/validation";
+import { ErrorObjectSchema } from "@/utils/error/schemas";
+import { setHttpResponseStatus } from "@/utils/http/set-response-status";
+import { err, ok } from "@/utils/result";
+
+export const reportErrorServerFn = createServerFn({ method: "POST" }).handler(
+  ({ data }): Result<undefined, FlatErrorObject<ValidationError["code"]>> => {
+    const parseResult = ErrorObjectSchema.safeParse(data);
+
+    if (parseResult.success) {
+      console.error(parseResult.data);
+      return ok(undefined);
+    }
+
+    const validationError = new ValidationError({ cause: parseResult.error });
+    console.error(validationError.deepSerialize());
+
+    setHttpResponseStatus("BAD_REQUEST");
+    return err(validationError.shallowSerialize());
+  },
+);
