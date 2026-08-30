88066f986643b13b0cb96e58f9d4ef9a59e214f0
Ryan Luong <lumintriet@gmail.com>
2026-08-30 07:36:22 +0700

    chore(deps): remove cloudflare.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-import { defineConfig, mergeConfig } from "vite";
+/// <reference types="vitest/config" />
 
-import { sharedConfig } from "./shared.config.ts";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default mergeConfig(
-  sharedConfig,
-  defineConfig({
-    plugins: [cloudflare({ viteEnvironment: { name: "ssr" } })],
-  }),
-);
+export default defineConfig({
+  plugins: [
+    // Make sure that '@tanstack/react-start/plugin/vite' is passed before '@vitejs/plugin-react'
+    tanstackStart({
+      router: {
+        quoteStyle: "double",
+        semicolons: true,
+      },
+    }),
+    babel({ presets: [reactCompilerPreset()] }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    port: 5173,
+  },
+  test: {
+    typecheck: {
+      enabled: true,
+    },
+  },
+});
