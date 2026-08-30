f86ceaa5d5233d8987d4c83f427d4c1e4cdb195f
Ryan Luong <lumintriet@gmail.com>
2026-08-30 08:11:14 +0700

    feat(auth/api): use the new authServer.

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -1,40 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { createDb } from "@/db";
-import { AuthEnvSchema } from "@/features/auth/schemas/env";
-import { createAuthServer } from "@/features/auth/server";
+import { authServer } from "@/features/auth/server";
 import { InternalServerError } from "@/utils/error/classes/http";
-import { InvalidEnvError } from "@/utils/error/classes/invalid-env";
 
 export const Route = createFileRoute("/api/auth/$")({
   server: {
     handlers: {
       ANY: async ({ request }) => {
-        const parseEnvResult = AuthEnvSchema.safeParse(process.env);
-        if (!parseEnvResult.success) {
-          const invalidEnvError = new InvalidEnvError({
-            cause: parseEnvResult.error,
-          });
-          const internalServerError = new InternalServerError({
-            cause: invalidEnvError,
-          });
-
-          console.error(internalServerError.deepSerialize());
-
-          return Response.json(internalServerError.shallowSerialize(), {
-            status: internalServerError.status.code,
-            statusText: internalServerError.status.text,
-          });
-        }
-
-        const env = parseEnvResult.data;
-        const db = createDb(env.NEON_POOLED_CONNECTION_STRING);
-        const authServer = createAuthServer({
-          db,
-          baseURL: import.meta.env.VITE_AUTH_BASE_URL,
-          secret: env.AUTH_SECRET,
-        });
-
         try {
           return await authServer.handler(request);
         } catch (error) {
