98870eab90b12ef21c3b71fd7fe4992f5b80dd9c
Ryan Luong <lumintriet@gmail.com>
2026-08-30 08:33:09 +0700

    feat(server): handle error in server entry.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
 
+import { UnexpectedError } from "@/utils/error/classes/unexpected";
+import { HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD } from "@/utils/http/constants/response-statuses/error/server";
+
 export default createServerEntry({
   fetch(request) {
-    return handler.fetch(request);
+    try {
+      return handler.fetch(request);
+    } catch (error) {
+      const unexpectedError = new UnexpectedError({
+        failedTo: "handle request",
+        cause: error,
+        context: { source: "src/server.ts" },
+      });
+
+      console.error(unexpectedError.deepSerialize());
+
+      const httpStatus =
+        HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD["INTERNAL_SERVER_ERROR"];
+      return Response.json(unexpectedError.shallowSerialize(), {
+        status: httpStatus.code,
+        statusText: httpStatus.text,
+      });
+    }
   },
 });
