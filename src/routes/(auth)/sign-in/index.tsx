7113246d4d4822a1e044c29b086faec2f82bbb41
Ryan Luong <lumintriet@gmail.com>
2026-08-31 17:11:52 +0700

    feat(auth/sign-in): handle success case on submit.

--- src/routes/(auth)/sign-in/index.tsx
+++ src/routes/(auth)/sign-in/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
+import { toast } from "@/components/ui/toast";
+import { authClient } from "@/features/auth/client";
 import { SignInParamsSchema } from "@/features/auth/schemas";
 import { useAppForm } from "@/lib/form/hooks";
 import { RedirectSearchParamSchema } from "@/utils/schemas/redirect";
@@ -22,6 +24,30 @@ function SignInPage() {
     },
     validators: {
       onSubmit: SignInParamsSchema,
+    },
+    onSubmit: async ({ value }) => {
+      // The SignInParamsSchema transforms email.
+      // However, TanStack Form doesn't use the output of validators for the value.
+      // So, we need to parse it again here to get the transformed email.
+      const { email, password, rememberMe } = SignInParamsSchema.parse(value);
+
+      // Sign in operation
+      const { data } = await authClient.signIn.email({
+        email,
+        password,
+        rememberMe,
+      });
+
+      // Handle success case
+      if (data) {
+        toast.add({
+          type: "success",
+          title: "Sign up succeeded",
+          description: `Welcome back, ${data.user.name}.`,
+        });
+        await navigate({ to: redirect ?? "/account" });
+        return;
+      }
     },
   });
 
