9c31648f89a141cbb26abd513aa9e910a8858d5e
Ryan Luong <lumintriet@gmail.com>
2026-08-31 08:22:26 +0700

    feat(auth/sign-up): add redirect search param.

--- src/routes/(auth)/sign-up/index.tsx
+++ src/routes/(auth)/sign-up/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { RedirectSearchParamSchema } from "@/utils/schemas/redirect";
+
 export const Route = createFileRoute("/(auth)/sign-up/")({
+  validateSearch: RedirectSearchParamSchema,
   component: SignUpPage,
 });
 
 function SignUpPage() {
+  const { redirect } = Route.useSearch();
+
   return <div>Hello "/(auth)/sign-up/"!</div>;
 }
