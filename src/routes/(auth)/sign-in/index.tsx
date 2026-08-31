1d0e0ffbd42529af757fa93691bcb6142dfcb0f8
Ryan Luong <lumintriet@gmail.com>
2026-08-31 17:05:18 +0700

    feat(auth/sign-in): add redirect utils.

--- src/routes/(auth)/sign-in/index.tsx
+++ src/routes/(auth)/sign-in/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+
+import { RedirectSearchParamSchema } from "@/utils/schemas/redirect";
 
 export const Route = createFileRoute("/(auth)/sign-in/")({
+  validateSearch: RedirectSearchParamSchema,
   component: SignInPage,
 });
 
 function SignInPage() {
+  const { redirect } = Route.useSearch();
+  const navigate = useNavigate({ from: "/sign-in/" });
+
   return <div>Hello "/(auth)/sign-in/"!</div>;
 }
