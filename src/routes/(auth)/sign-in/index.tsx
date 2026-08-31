845536281a141ab12cc36f4669daa9bab699b3c5
Ryan Luong <lumintriet@gmail.com>
2026-08-31 17:08:29 +0700

    feat(auth/sign-in): create signInForm.

--- src/routes/(auth)/sign-in/index.tsx
+++ src/routes/(auth)/sign-in/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
+import { SignInParamsSchema } from "@/features/auth/schemas";
+import { useAppForm } from "@/lib/form/hooks";
 import { RedirectSearchParamSchema } from "@/utils/schemas/redirect";
 
 export const Route = createFileRoute("/(auth)/sign-in/")({
@@ -10,6 +12,18 @@ export const Route = createFileRoute("/(auth)/sign-in/")({
 function SignInPage() {
   const { redirect } = Route.useSearch();
   const navigate = useNavigate({ from: "/sign-in/" });
+
+  const signInForm = useAppForm({
+    formId: "sign-in-form",
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: true,
+    },
+    validators: {
+      onSubmit: SignInParamsSchema,
+    },
+  });
 
   return <div>Hello "/(auth)/sign-in/"!</div>;
 }
