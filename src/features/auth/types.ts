eea319b5145808867632b3bf8acfddf9e679a427
Ryan Luong <lumintriet@gmail.com>
2026-09-01 09:14:34 +0700

    refactor(auth/types): remove unused type.

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -1,6 +1,4 @@
 import type { authServer } from "@/features/auth/server";
 
-export type AuthErrorCode = keyof typeof authServer.$ERROR_CODES;
-
 export type AuthSession = typeof authServer.$Infer.Session;
 export type AuthUser = typeof authServer.$Infer.Session.user;
